4f89e7f4bf077bc9c4b575ae63fb73e4a03da70e
Felix <malsch.felix@web.de>
2023-12-09 17:37:11 +0100

    training with human input runs better state preprocessing happens outside of DoubleDeepQAgent

--- DeepQCarRacing/DoubleDeepQAgent.py
+++ DeepQCarRacing/DoubleDeepQAgent.py
@@ -2,12 +2,18 @@ import numpy as np
 
 import DeepQNet as dqn
 
-import ObservationProcessor as op
-
 class DoubleDeepQAgent:
     
+    def _reset_training_samples(self):
+        self.training_states = []
+        self.training_actions = []
+        self.training_rewards = []
+        self.training_next_states = []
+    
     def __init__(self, 
-        env, 
+        env,
+        state_shape,
+        action_shape,
         layer_sizes, 
         activation_functions, 
         init, 
@@ -19,11 +25,8 @@ class DoubleDeepQAgent:
         epsilon_decay):
 
         self.env = env
-        
-        state = self.env.observation_space.sample()
-        self.state_shape = (len(op.ObservationProcessor.get_state(state)), )
-        
-        self.action_shape = (env.action_space.n, )
+        self.state_shape = state_shape
+        self.action_shape = action_shape
 
         self.epochs = epochs      
         
@@ -34,8 +37,7 @@ class DoubleDeepQAgent:
             self.qNet = dqn.DeepQNet(self.state_shape, self.action_shape, layer_sizes, activation_functions, init, learning_rate, self.epochs)
             self.targetNet = dqn.DeepQNet(self.state_shape, self.action_shape, layer_sizes, activation_functions, init, learning_rate, self.epochs)
         
-        self.training_states= []
-        self.training_qValues = []
+        self._reset_training_samples()
 
         self.training_iteration = training_iteration
         self.weights_transfer_iteration = weights_transfer_iteration
@@ -48,7 +50,9 @@ class DoubleDeepQAgent:
 
     @staticmethod
     def load(
-        env, 
+        env,
+        state_shape,
+        action_shape,
         path,
         epochs,
         training_iteration, 
@@ -56,7 +60,7 @@ class DoubleDeepQAgent:
         gamma, 
         epsilon_decay):
 
-        model = DoubleDeepQAgent(env, *[None]*4, epochs, training_iteration, weights_transfer_iteration, gamma, epsilon_decay)
+        model = DoubleDeepQAgent(env, state_shape, action_shape, *[None]*4, epochs, training_iteration, weights_transfer_iteration, gamma, epsilon_decay)
         
         model.qNet = dqn.DeepQNet.load(path, model.epochs)
         model.targetNet = dqn.DeepQNet.load(path, model.epochs)
@@ -64,9 +68,7 @@ class DoubleDeepQAgent:
         return model
     
     def get_Q_values(self, state):
-        processed_state = op.ObservationProcessor.get_state(state)
-        
-        return self.qNet.run(processed_state)
+        return self.qNet.run(state)
     
     def get_action(self, state):        
         return np.argmax(self.get_Q_values(state))
@@ -78,26 +80,35 @@ class DoubleDeepQAgent:
             return self.env.action_space.sample()
         else:
             return self.get_action(state)
-            
-            
+             
         
-    def train(self, state, action, reward, next_state):
-        processed_state = op.ObservationProcessor.get_state(state)
-        processed_next_state = op.ObservationProcessor.get_state(next_state)
-        
-        target_vector = self.get_Q_values(state)[0]
-        target_vector[action] = reward + self.gamma * self.targetNet.run(processed_next_state)[0][action] ###########################
-        
-        self.training_states.append(processed_state)
-        self.training_qValues.append(target_vector)
+    def train(self, state, action, reward, next_state):             
+        self.training_states.append(state)
+        self.training_actions.append(action)
+        self.training_rewards.append(reward)
+        self.training_next_states.append(next_state)
         
         if self.numIterations % self.training_iteration == 0:
+
+            training_targets = []
+
+            for i in range(len(self.training_states)):
+                state = self.training_states[i]
+                action = self.training_actions[i]
+                reward = self.training_rewards[i]
+                next_state = self.training_next_states[i]
+
+                target_vector = self.get_Q_values(state)[0]
+                target_vector[action] = reward + self.gamma * self.targetNet.run(next_state)[0][action]
+
+                training_targets.append(target_vector)
+
             training_states = np.array(self.training_states)
-            training_qValues = np.array(self.training_qValues)
+            training_targets = np.array(training_targets)
             
-            self.qNet.train(training_states, training_qValues)
-            self.training_states = []
-            self.training_qValues = []
+            self.qNet.train(training_states, training_targets)
+
+            self._reset_training_samples()
             
         if self.numIterations % self.weights_transfer_iteration == 0:
             self.targetNet.set_weights(self.qNet.get_weights())
--- DeepQCarRacing/Main.py
+++ DeepQCarRacing/Main.py
@@ -52,6 +52,7 @@ def run(env, ddqAgent, num_iterations, train = True):
     get_action = ddqAgent.get_action_epsilon_greedy if train else ddqAgent.get_action
 
     state, info = env.reset()
+    state = op.ObservationProcessor.get_state(state)
     
     for i in range(num_iterations):
         
@@ -64,8 +65,9 @@ def run(env, ddqAgent, num_iterations, train = True):
         action = human_action if interactive else get_action(state)
         
         next_state, reward, terminated, truncated, info = env.step(action)
+        next_state = op.ObservationProcessor.get_state(next_state)
         
-        print('state: ', op.ObservationProcessor.get_state(state))
+        print('state: ', state)
         print('action: ', action, 'reward: ', reward)
     
         if train:
@@ -75,6 +77,7 @@ def run(env, ddqAgent, num_iterations, train = True):
         
         if terminated or truncated:
             state, info = env.reset()
+            state = op.ObservationProcessor.get_state(state)
 
     env.close()
     
@@ -86,8 +89,15 @@ def main(human_input = True, train = False, load_network = True):
     
     env = gym.make("CarRacing-v2", continuous = False, render_mode = "human") if human_input else\
           gym.make("CarRacing-v2", continuous = False)
+
+    state = env.observation_space.sample()
+    state = op.ObservationProcessor.get_state(state)
+    state_shape = (len(state), )
+    action_shape = (env.action_space.n, )
     
     ddqAgent = ddqa.DoubleDeepQAgent.load(env,
+                                          state_shape,
+                                          action_shape,
                                           hp.FILENAME,
                                           hp.EPOCHS,
                                           hp.TRAINING_ITERATION, 
@@ -95,6 +105,8 @@ def main(human_input = True, train = False, load_network = True):
                                           hp.GAMMA, 
                                           hp.EPSILON_DECAY) if load_network else\
                ddqa.DoubleDeepQAgent(     env, 
+                                          state_shape,
+                                          action_shape,
                                           hp.LAYER_SIZES, 
                                           hp.LAYER_ACTIVATIONS, 
                                           tf.keras.initializers.Zeros(), 
