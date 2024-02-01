b8e2900e9014f112a51e25838bf2bc181eb41372
HQNA-git <f.schubertHAW@web.de>
2024-02-01 12:52:44 +0100

    rewardlists and praesi live demo

--- __pycache__/Agent_no_pretrainv2.py
+++ __pycache__/Agent_no_pretrainv2.py
@@ -198,7 +198,7 @@ activation_functions = ['relu', 'relu', 'relu', 'relu', 'relu', 'linear']
 
 # RL - Variables
 epsilon = 0.98
-EPSILON_DECAY = 0.993
+EPSILON_DECAY = 0.99
 GAMMA = 0.98
 ACTION_SIZE = 5
 
@@ -250,7 +250,7 @@ observation, info = env.reset()
 
 episodes = 200
 timesteps = 1000
-model_iteration = 102
+model_iteration = 1602
 
 # Load the pre-trained RL model if available
 #rl_model_filename = rl_model_filename_template.format(model_iteration)
@@ -277,14 +277,14 @@ for epoch in range(50):
             state = np.array(processor.get_state(observation))
 
             if np.random.rand() < epsilon:
-                    if trainer.get_speed(observation) == 5 and action == 3:
+                    if trainer.get_speed(observation) == 5 and action == 3: #try everything but
                         while action == 3:
                             action = np.random.choice(ACTION_SIZE)
-                    elif trainer.get_speed(observation) == 4:
+                    elif trainer.get_speed(observation) == 4:           # try everything
                         action = np.random.choice(ACTION_SIZE)
-                    elif trainer.get_speed(observation) == 3:
-                        action = np.random.choice(ACTION_SIZE - 2) + 1  # only gas and steering
-                    elif trainer.get_speed(observation) < 3:
+                    elif trainer.get_speed(observation) == 3:           # only try Gas and Steering
+                        action = np.random.choice(ACTION_SIZE - 2) + 1  
+                    elif trainer.get_speed(observation) < 3:            # only try Gas
                         action = 3
             else:
                 action = np.argmax(model.predict(state.reshape(1, *state.shape), verbose=None)[0])
@@ -342,153 +342,230 @@ print("RL Training done")
 
 #%% rl triaining mid
 
-# RL-training
+# RL-training mid after 1600 episodes ##### after 1772 start epsilon 0.3 #### ab 1892 0.2 ### 2182  0.15 §§2392 0.1
 print("Start RL training")
 env = gym.make("CarRacing-v2", render_mode = "human", continuous=False)   #render_mode = "human",
 observation, info = env.reset()
 
-episodes = 1000
-timesteps = 1000
-model_iteration = 1000
+episodes = 200
+timesteps = 3000
+model_iteration = 2492
 
 # Load the pre-trained RL model if available
 #rl_model_filename = rl_model_filename_template.format(model_iteration)
 
 model = keras.models.load_model(f"RL_Agent_vB{model_iteration}.keras")
+#model_iteration = 102
 # RL training loop
+
 memory_buffer = {'states': [], 'actions': [], 'rewards': []}
-processor = Trainingsdata_generator()
+processor = Agent_state_Processor()
+trainer = Trainingsdata_generator()
 total_reward = 0
-for episode in range(episodes):
-    
 
-    total_reward = 0
-    rand = np.random.rand()
+for epoch in range(50):
+    epsilon = 0.15
+    for episode in range(episodes):
+        
+        total_reward = 0
+        
+        if epsilon > 0.01:
+            epsilon *= EPSILON_DECAY
 
-    if epsilon > 0.01:
-        epsilon *= EPSILON_DECAY
+        for timestep in range(timesteps):
+            state = np.array(processor.get_state(observation))
 
-    for timestep in range(timesteps):
-        state = np.array(processor.get_state(observation))
-
-        if timestep % 4 == 0:
-            rand = np.random.rand()
-
-        if np.random.rand() < epsilon:
-            if rand < 0.5:
+            if np.random.rand() < epsilon:
+                    if trainer.get_speed(observation) > 3:
+                        action = np.random.choice(ACTION_SIZE)
+                    elif trainer.get_speed(observation) == 3:
+                        action = np.random.choice(ACTION_SIZE - 2) + 1  # only gas and steering
+                    elif trainer.get_speed(observation) < 3:
                         action = 3
-            elif rand > 0.90:
-                if rand < 0.95:
-                        action = 0
-                else:
-                        action = 4
             else:
-                if rand > 0.7:
-                        action = 1
-                else:
-                        action = 2
+                action = np.argmax(model.predict(state.reshape(1, *state.shape), verbose=None)[0])
 
-            '''
-            if trainer.get_speed(observation) == 5 and action == 3:
-                    while action == 3:
-                        action = np.random.choice(ACTION_SIZE)
-            elif trainer.get_speed(observation) == 4:
-                    action = np.random.choice(ACTION_SIZE)
-            elif trainer.get_speed(observation) == 3:
-                    action = np.random.choice(ACTION_SIZE - 2) + 1  # only gas and steering
-            elif trainer.get_speed(observation) < 3:
-                    action = 3
-            '''
-            '''
-            if trainer.get_speed(observation) == 5 and action == 3:
-                while action == 3:
-                    action = np.random.choice(ACTION_SIZE)
-            elif trainer.get_speed(observation) == 4:
-                action = np.random.choice(ACTION_SIZE)
-            elif trainer.get_speed(observation) == 3:
-                action = np.random.choice(ACTION_SIZE - 2) + 1  # only gas and steering
-            elif trainer.get_speed(observation) < 3:
-                action = 3
+            observation, reward, terminated, truncated, info = env.step(action)
+            total_reward += reward
+
+
+            if timestep > 15 and trainer.get_vision(observation)[0] == 1:
+                reward = -2
+                terminated = True
+
+            next_state = np.array(processor.get_state(observation))
             
-            if timestep > 30:
-                if sum(trainer.get_state(observation)[28:33]) < sum(trainer.get_state(observation)[58:63]):
-                    if np.random.rand() < 0.2:
-                        action = np.random.choice(ACTION_SIZE)
-                    else:
-                        action = 1
+            # Store data in the memory buffer
+            memory_buffer['states'].append(state)
+            memory_buffer['actions'].append(action)
+            memory_buffer['rewards'].append(reward)
+
+            state = next_state
+
+            if terminated or truncated:
+                observation, info = env.reset()
+                break
+        print(f"Episode {episode + 1} done! , Epsilon: {epsilon}, Total Reward {total_reward}")        
+        rl_reward_list.append(total_reward)
         
-                if sum(trainer.get_state(observation)[28:33]) > sum(trainer.get_state(observation)[58:63]):
-                    if np.random.rand() < 0.2:
-                        action = np.random.choice(ACTION_SIZE)
-                    else:
-                        action = 2
+        # RL train the agent periodically using the memory buffer
+        if (episode + 1) % 5 == 0:
+            state_array = np.array(memory_buffer['states'])
+            action_array = np.array(memory_buffer['actions'])
+            reward_array = np.array(memory_buffer['rewards'])
 
-            '''
-        else:
-            action = np.argmax(model.predict(state.reshape(1, *state.shape), verbose=None)[0])
+            q_values = model.predict(state_array, verbose=None)
+            target_values = q_values.copy()
 
-        observation, reward, terminated, truncated, info = env.step(action)
-        total_reward += reward
+            for i in range(len(state_array) - 1):
+                target_values[i, action_array[i]] = reward_array[i] + GAMMA * np.max(q_values[i + 1])
+
+        # Train the model with the accumulated data
+            model.fit(state_array, target_values, batch_size=10, epochs=1, verbose=None)
+
+            #model.save(f"true_rl_agent_iteration_{model_iteration}")
+
+            # Clear the memory buffer
+            memory_buffer = {'states': [], 'actions': [], 'rewards': []}
+        if (episode + 1) % 10 == 0:
+            model_iteration += 10
+            model.save(f"RL_Agent_vB{model_iteration}.keras")  
+            np.save(f"rl_reward_list_vB{model_iteration}.npy", rl_reward_list)
         
-        if reward > 0:
-            reward *= processor.get_speed(observation)
-
-        if timestep > 30 and processor.get_vision(observation)[0] == 1:
-            terminated = True
-
-        next_state = np.array(processor.get_state(observation))
-       
-        # Store data in the memory buffer
-        memory_buffer['states'].append(state)
-        memory_buffer['actions'].append(action)
-        memory_buffer['rewards'].append(reward)
-
-        state = next_state
-
-        if terminated or truncated:
-            observation, info = env.reset()
-            break
-    print(f"Episode {episode + 1} done! , Epsilon: {epsilon}, Total Reward {total_reward}")        
-    rl_reward_list.append(total_reward)
-    
-    # RL train the agent periodically using the memory buffer
-    if (episode + 1) % 5 == 0:
-        state_array = np.array(memory_buffer['states'])
-        action_array = np.array(memory_buffer['actions'])
-        reward_array = np.array(memory_buffer['rewards'])
-
-        q_values = model.predict(state_array, verbose=None)
-        target_values = q_values.copy()
-
-        for i in range(len(state_array) - 1):
-            target_values[i, action_array[i]] = reward_array[i] + GAMMA * np.max(q_values[i + 1])
-
-       # Train the model with the accumulated data
-        model.fit(state_array, target_values, batch_size=10, epochs=1, verbose=None)
-
-        #model.save(f"true_rl_agent_iteration_{model_iteration}")
-
-        # Clear the memory buffer
-        memory_buffer = {'states': [], 'actions': [], 'rewards': []}
-    if (episode + 1) % 50 == 0:
-        model_iteration += 1
-        model.save(f"RL_Agent_vB{model_iteration}.keras")  
-        np.save(f"rl_reward_list_vB{model_iteration}.npy", rl_reward_list)
-    
 
 env.close()
 print("RL Training done")
+
+
+#%% Late training
+# RL-training late with bonus speed reward 2502 and higher
+print("Start RL training")
+env = gym.make("CarRacing-v2", render_mode = "human", continuous=False)   #render_mode = "human",
+observation, info = env.reset()
+
+episodes = 200
+timesteps = 3000
+model_iteration = 2492
+
+# Load the pre-trained RL model if available
+#rl_model_filename = rl_model_filename_template.format(model_iteration)
+
+model = keras.models.load_model(f"RL_Agent_vB{model_iteration}.keras")
+#model_iteration = 102
+# RL training loop
+
+memory_buffer = {'states': [], 'actions': [], 'rewards': []}
+processor = Agent_state_Processor()
+trainer = Trainingsdata_generator()
+total_reward = 0
+
+for epoch in range(50):
+    epsilon = 0.4
+    for episode in range(episodes):
+        
+        total_reward = 0
+        
+        if epsilon > 0.01:
+            epsilon *= EPSILON_DECAY
+
+        for timestep in range(timesteps):
+            state = np.array(processor.get_state(observation))
+
+            if np.random.rand() < epsilon:
+                    if trainer.get_speed(observation) > 3:
+                        action = np.random.choice(ACTION_SIZE)
+                    elif trainer.get_speed(observation) == 3:
+                        action = np.random.choice(ACTION_SIZE - 2) + 1  # only gas and steering
+                    elif trainer.get_speed(observation) < 3:
+                        action = 3
+            else:
+                action = np.argmax(model.predict(state.reshape(1, *state.shape), verbose=None)[0])
+
+            observation, reward, terminated, truncated, info = env.step(action)
+            total_reward += reward
+
+            if reward > 0:
+                reward += trainer.get_speed(observation)
+
+            if timestep > 15 and trainer.get_vision(observation)[0] == 1:
+                reward = -2
+                terminated = True
+
+            next_state = np.array(processor.get_state(observation))
+            
+            # Store data in the memory buffer
+            memory_buffer['states'].append(state)
+            memory_buffer['actions'].append(action)
+            memory_buffer['rewards'].append(reward)
+
+            state = next_state
+
+            if terminated or truncated:
+                observation, info = env.reset()
+                break
+        print(f"Episode {episode + 1} done! , Epsilon: {epsilon}, Total Reward {total_reward}")        
+        rl_reward_list.append(total_reward)
+        
+        # RL train the agent periodically using the memory buffer
+        if (episode + 1) % 5 == 0:
+            state_array = np.array(memory_buffer['states'])
+            action_array = np.array(memory_buffer['actions'])
+            reward_array = np.array(memory_buffer['rewards'])
+
+            q_values = model.predict(state_array, verbose=None)
+            target_values = q_values.copy()
+
+            for i in range(len(state_array) - 1):
+                target_values[i, action_array[i]] = reward_array[i] + GAMMA * np.max(q_values[i + 1])
+
+        # Train the model with the accumulated data
+            model.fit(state_array, target_values, batch_size=10, epochs=1, verbose=None)
+
+            #model.save(f"true_rl_agent_iteration_{model_iteration}")
+
+            # Clear the memory buffer
+            memory_buffer = {'states': [], 'actions': [], 'rewards': []}
+        if (episode + 1) % 10 == 0:
+            model_iteration += 10
+            model.save(f"RL_Agent_vB{model_iteration}.keras")  
+            np.save(f"rl_reward_list_vB{model_iteration}.npy", rl_reward_list)
+        
+
+env.close()
+print("RL Training done")
+
 #%% Plot
+def get_last_100_elements(input_list):
+    if len(input_list) <= 100:
+        return input_list
+    else:
+        return input_list[-100:]
+    
+def get_last_10_elements(input_list):
+    if len(input_list) <= 10:
+        return input_list
+    else:
+        return input_list[-10:]
+
 plotlist = []
 model_iteration = 100
-while True:
+while model_iteration < 2200: 
     loadlist = np.load(f"rl_reward_list_vB{model_iteration}.npy")
-    for i in loadlist:
+    fixedlist = get_last_100_elements(loadlist)
+    for i in fixedlist:
         plotlist.append(i)
-    model_iteration += 100
 
-    if model_iteration == 2200:
-        break
+    model_iteration += 100
+'''
+while model_iteration < 2512: 
+    loadlist = np.load(f"rl_reward_list_vB{model_iteration}.npy")
+    fixedlist = get_last_10_elements(loadlist)
+    for i in fixedlist:
+        plotlist.append(i)
+
+    model_iteration += 10
+print(max(plotlist))
+'''
 
 plt.plot(plotlist, label="total_RL")
 plt.ylim(-200, 1000)
@@ -499,32 +576,27 @@ plt.show()
 
 #%% Some Display of Skill
 env = gym.make("CarRacing-v2", render_mode="human", continuous=False)
-observation, info = env.reset()
 
-model_iteration = 2200
-model = keras.models.load_model(f"RL_Agent_v{model_iteration}.keras")
-# model = model.load(rl_model_filename)
-
-episodes = 1000
-timesteps = 1000
+model_iteration = 2252
+model = keras.models.load_model(f"RL_Agent_vB{model_iteration}.keras")
+processor = Agent_state_Processor()
+episodes = 100
+timesteps = 2000
 
 print("Some Display of Skill")
-# Loop to demonstrate the agent's skill
 for episode in range(episodes):
     total_reward = 0
+    observation, info = env.reset()
     for timestep in range(timesteps):
         state = np.array(processor.get_state(observation))
         action = np.argmax(model.predict(state.reshape(1, *state.shape), verbose=None)[0])
         observation, reward, terminated, truncated, info = env.step(action)
         next_state = np.array(processor.get_state(observation))
         total_reward += reward
-
-        # Update the current state
         state = next_state
-
         if terminated or truncated:
-            observation, info = env.reset()
             break
+    print(f"Episode: {episode} done! TR: {total_reward}")
 env.close()
 
 # %%
